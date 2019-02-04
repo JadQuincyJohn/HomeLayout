da86307c216d1c831f29ed10e8609ea3dcc616b2
Jad Abi-Abdallah <jad.abiabdallah@publicis.sapient.com>
2019-02-04 13:20:05 +0100

    Initial Commit

---
+++ HomeLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeLayout
+//
+//  Created by Jad Abi-Abdallah on 04/02/2019.
+//  Copyright © 2019 Jad Abi-Abdallah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
